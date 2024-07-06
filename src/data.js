42a74289d673098482b76abd7b377cb5ca1e795c
Harsh Gaur <harshgr441@gmail.com>
2024-07-06 21:05:03 +0530

    minor change3

--- src/data.js
+++ src/data.js
@@ -171,7 +171,7 @@ export const resume = [
     category: 'achievement',
     icon: <BsFillAwardFill />,
     year: 'Current',
-    title: 'Competetive Programing/DSA<span>(700+ problems solved)</span>',
+    title: 'Competitive Programing/DSA<span>(700+ problems solved)</span>',
     desc: 'Codeforces(1323), Codechef(1688), Leetcode(1622).',
   },
 
